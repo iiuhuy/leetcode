9c63a488bee43510b347c66f22a50cbfacee1497
AlvinMi <alvin.mi0620@gmail.com>
2017-04-18 23:29:31 +0800

    c: A+B 问题

---
+++ hihocode/A+B.c
@@ -0,0 +1,17 @@
+//实现一个简单的 A+B 问题
+//程序实现了 键盘输入两个 0~100 内的数字
+//输出 两次输入相加的和。
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	//while(scanf("%d%d", &a, &b) != EOF)
+	//{
+		scanf("%d%d", &a, &b) != EOF;
+		printf("%d\n", a + b);
+	//}
+	return 0;
+}
+
