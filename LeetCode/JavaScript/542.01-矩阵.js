7029e6a90a7b8810bacc2e33c7bdf8532b9378a0
AlvinMi <alvin.mi0620@gmail.com>
2020-04-15 23:31:44 +0800

    feat: 🎸 542. 01 矩阵

---
+++ LeetCode/JavaScript/542.01-矩阵.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=542 lang=javascript
+ *
+ * [542] 01 矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var updateMatrix = function(matrix) {
+  let rows = matrix.length,
+    cols = matrix[0].length;
+  let dp = new Array(rows).fill(10000).map(() => new Array(cols).fill(10000));
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (matrix[r][c] === 0) dp[r][c] = 0;
+      else {
+        if (r > 0) dp[r][c] = Math.min(dp[r][c], dp[r - 1][c] + 1);
+        if (c > 0) dp[r][c] = Math.min(dp[r][c], dp[r][c - 1] + 1);
+      }
+    }
+  }
+
+  for (let r = rows - 1; r >= 0; r--) {
+    for (let c = cols - 1; c >= 0; c--) {
+      if (r < rows - 1) dp[r][c] = Math.min(dp[r][c], dp[r + 1][c] + 1);
+      if (c < cols - 1) dp[r][c] = Math.min(dp[r][c], dp[r][c + 1] + 1);
+    }
+  }
+  return dp;
+};
+// @lc code=end
