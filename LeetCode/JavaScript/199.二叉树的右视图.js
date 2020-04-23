35744c5395f4bb6e32753582e7dfff51342cc985
AlvinMi <alvin.mi0620@gmail.com>
2020-04-23 23:44:15 +0800

    面试题:08.11.硬币.js

--- LeetCode/JavaScript/199.二叉树的右视图.js
+++ LeetCode/JavaScript/199.二叉树的右视图.js
@@ -19,20 +19,20 @@
 var rightSideView = function(root) {
   if (!root) return [];
   let arr = [];
+  function dfs(root, step, res) {
+    if (root) {
+      if (res.length === step) {
+        res.push(root.val); // 当数组长度等于当前 深度 时, 把当前的值加入数组
+      }
+      // console.log(step, '-------', res)
+      dfs(root.right, step + 1, res); // 先从右边开始, 当右边没了, 再轮到左边
+      dfs(root.left, step + 1, res);
+    }
+  }
 
   dfs(root, 0, arr);
 
   return arr;
 };
-function dfs(root, step, res) {
-  if (root) {
-    if (res.length === step) {
-      res.push(root.val); // 当数组长度等于当前 深度 时, 把当前的值加入数组
-    }
-    // console.log(step, '-------', res)
-    dfs(root.right, step + 1, res); // 先从右边开始, 当右边没了, 再轮到左边
-    dfs(root.left, step + 1, res);
-  }
-}
 console.log(rightSideView([1, 2, 3, null, 5, null, 4]));
 // @lc code=end
