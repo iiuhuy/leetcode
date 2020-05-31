43cdfda4fb267fe49abc63560abe1f249a9a16a2
AlvinMi <alvin.mi0620@gmail.com>
2020-05-31 23:52:04 +0800

    101. 对称二叉树

---
+++ LeetCode/JavaScript/101.对称二叉树.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=javascript
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  const isMirror = (t1, t2) => {
+    if (t1 == null && t2 == null) return true;
+    if (t1 == null || t2 == null) return false;
+
+    return (
+      t1.val === t2.val &&
+      isMirror(t1.left, t2.right) &&
+      isMirror(t1.right, t2.left)
+    );
+  };
+
+  return isMirror(root, root);
+};
+// @lc code=end
