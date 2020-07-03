9a6586d3b44bbee03f4646415f520d851470ad36
AlvinMi <alvin.mi0620@gmail.com>
2020-07-03 23:38:30 +0800

    108.将有序数组转换为二叉搜索树

---
+++ LeetCode/JavaScript/108.将有序数组转换为二叉搜索树.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=javascript
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  // 将有序的数组分成两半
+  // 递归完成操作
+  if (nums.length === 0) return null;
+  const mid = nums.length >> 1;
+  const root = new TreeNode(nums[mid]);
+
+  root.left = sortedArrayToBSTq(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
+};
+// @lc code=end
