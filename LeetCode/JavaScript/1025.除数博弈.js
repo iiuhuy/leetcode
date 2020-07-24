ea07dc297351bcb54652d4e93b40ccb41e3e2c5e
AlvinMi <alvin.mi0620@gmail.com>
2020-07-24 23:57:44 +0800

    1025.除数博弈

---
+++ LeetCode/JavaScript/1025.除数博弈.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=javascript
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function (N) {
+  if (N == 1) {
+    return false;
+  }
+  if (N == 2) {
+    return true;
+  }
+  const dp = new Array(N + 1);
+  dp[1] = false;
+  dp[2] = true;
+  for (let i = 3; i <= N; i++) {
+    dp[i] = false;
+    for (let j = 1; j < i; j++) {
+      // 如果i-j是false，而i本身是true，直接返回true
+      if (i % j === 0 && !dp[i - j]) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[N];
+};
+// @lc code=end
